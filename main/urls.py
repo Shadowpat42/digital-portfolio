2013cd2ed224447b27573b745a42b8b99ff10b63
Vladimir_Djobirov <djobirov05@mail.ru>
2024-05-09 13:18:48 +0500

    add index, event, methodological_resources, signup, login, user, react_to_post

---
+++ main/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("user/create", views.user_create, name="user_create"),
+    path("event", views.event, name="event"),
+    path("get_post/<int:post_id>", views.get_post, name="get_post"),
+    path("methodological_resources", views.methodological_resources, name="methodological_resources"),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path("user/<int:user_id>", views.user, name="user"),
+    path('react_to_post/<int:post_id>/', views.react_to_post, name='react_to_post'),
+]
