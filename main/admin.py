206211e3929b1ef8d1f7b73534019f614c8c7feb
Vladimir_Djobirov <djobirov05@mail.ru>
2024-06-07 04:09:28 +0500

    add class comment in admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import CustomUser, Post, MethodologicalResource, Reaction, Media
+from .models import CustomUser, Post, MethodologicalResource, Reaction, Media, Comment
 
 
 admin.site.register(CustomUser)
@@ -7,3 +7,4 @@ admin.site.register(Post)
 admin.site.register(MethodologicalResource)
 admin.site.register(Reaction)
 admin.site.register(Media)
+admin.site.register(Comment)
